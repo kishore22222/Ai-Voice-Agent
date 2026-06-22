0638ef5b54c5dafc3378536670f4b29053bb1a8f
kishore22222 <160571443+kishore22222@users.noreply.github.com>
2026-06-22 17:45:25 +0530

    Update auth.controller.js

--- Server/Controllers/auth.controller.js
+++ Server/Controllers/auth.controller.js
@@ -14,9 +14,9 @@ export const googleAuth = async (req,res)=>{
         }
         const token = await genToken(user._id)
         res.cookie("token",token,{
-            httpOnly:true,
-            secure:false,
-            sameSite:"Strict",
+            httpOnly:false,
+            secure:true,
+            sameSite:"none",
                 maxAge:7*24*60*60*1000
         })
         
@@ -30,9 +30,9 @@ export const googleAuth = async (req,res)=>{
 export const logOut = async (req,res) => {
     try {
         res.clearCookie("token",{
-            httpOnly:true,
-            secure:false,
-            sameSite:"Strict",
+            httpOnly:false,
+            secure:true,
+            sameSite:"none",
         })
         return res.status(200).json({message:"Logout succesfully"})
         
