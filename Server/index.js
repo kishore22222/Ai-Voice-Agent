7bcb2f96a5bcc76601533261c1a03ac1ebbd569d
kishore22222 <160571443+kishore22222@users.noreply.github.com>
2026-06-22 18:27:47 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -1,45 +1,42 @@
 import express from "express"
 import dotenv from "dotenv"
-import connectDB from "./Configs/ConnectDB.js";
-import authRouter from "./Routes/auth.route.js";
-import cookieParser from "cookie-parser";
+import connectDB from "./Configs/ConnectDB.js"
+import authRouter from "./Routes/auth.route.js"
+import cookieParser from "cookie-parser"
 import cors from "cors"
-import userRouter from "./Routes/user.route.js";
-import assistantRouter from "./Routes/assistant.route.js";
-import { getAssistantConfig } from "./Controllers/assistant.controller.js"
-import billingRouter from "./Routes/billing.route.js";
-dotenv.config();
-
+import userRouter from "./Routes/user.route.js"
+import assistantRouter from "./Routes/assistant.route.js"
+import billingRouter from "./Routes/billing.route.js"
+dotenv.config()
 
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
+
 const privateCors = cors({
-    origin:[
-        "https://shifra-d82z.onrender.com",
-        "https://ai-voice-agentserver.onrender.com"
+    origin: [
+        "http://localhost:5173",
+        "https://shifra-d82z.onrender.com"
     ],
-    credentials:true
-});
+    credentials: true
+})
+
 const publicCors = cors({
+    origin: "*"
+})
 
-    origin:"*"
-});
-
-
-
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("hello from server")
 })
-app.use("/api/auth",privateCors,authRouter)
-app.use("/api/user",privateCors,userRouter)
-app.use("/api/billing",privateCors,billingRouter)
 
-app.use("/api/assistant",publicCors,assistantRouter)
+app.use("/api/auth", privateCors, authRouter)
+app.use("/api/user", privateCors, userRouter)
+app.use("/api/billing", privateCors, billingRouter)
+app.use("/api/assistant", publicCors, assistantRouter)
+
 const PORT = process.env.PORT
 
-app.listen(PORT,()=>{
-    console.log(`server started on port  ${PORT}`);
+app.listen(PORT, () => {
+    console.log(`server started on port ${PORT}`)
     connectDB()
-    
 })
